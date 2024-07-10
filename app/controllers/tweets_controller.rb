5ac5ad0c98f3adb671b2c7eb66a6c5c3cd242c8b
h8rry <harrychenca@gmail.com>
2024-07-09 18:46:57 -0700

    Fixes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,51 +1,56 @@
 class TweetsController < ApplicationController
-    def index
-        @tweets = Tweet.all
-        render 'tweets/index'
+  def index
+    @tweets = Tweet.all.order(created_at: :desc)
+    render 'tweets/index'
+  end
+
+  def index_by_user
+    # TODO: remove this
+    token = cookies.permanent.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    # TODO: find user by username, not by current user
+
+    if session
+      @tweets = session.user.tweets
+      render 'tweets/index'
+    else
+      render json: { tweets: [] }
     end
+  end
 
-    def index_by_user
-        token = cookies.signed[:twitter_session_token]
-        session = Session.find_by(token: token)
+  def create
+    token = cookies.permanent.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
 
-        if session
-            @tweets = session.user.tweets
-            render 'tweets/index'
-        else
-            render json: { tweets: [] }
-        end
+    if session
+      user = session.user
+      @tweet = user.tweets.new(tweet_params)
+
+      if @tweet.save
+        render 'tweets/create'
+      else
+        render json: { success: false }
+      end
+    else
+      render json: { success: false }
     end
+  end
 
-    def create
-        token = cookies.signed[:twitter_session_token]
-        session = Session.find_by(token: token)
+  def destroy
+    # TODO: get current user and check if the user is the owner of the tweet
 
-        if session
-            user = session.user
-            @tweet = user.tweets.new(tweet_params)
-
-            if @tweet.save
-                render 'tweets/create'
-            else
-                render json: { success: false }
-            end
-        else
-            render json: { success: false }
-        end
+    @tweet = Tweet.find_by(id: params[:id])
+    if @tweet&.destroy
+      render json: { success: true }
+    else
+      render json: { success: false }
     end
+  end
 
-    def destroy
-        @tweet = Tweet.find_by(id: params[:id])
-        if @tweet&.destroy
-            render json: { success: true }
-        else
-            render json: { success: false}
-        end
-    end
+  private
 
-    private
-
-    def tweet_params
-        params.require(:tweet).permit(:message)
-    end
+  def tweet_params
+    params.require(:tweet).permit(:message)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
-    has_many :sessions
-    has_many :tweets
-    validates :username, presence: true, length: { minimum: 3, maximum: 64 }
-    validates_uniqueness_of :username
-    validates :email, presence: true, length: { minimum: 5, maximum: 500 }
-    validates :password, presence: true, length: { minimum: 8, maximum: 64 }
-    before_create :hash_password
+  has_many :sessions
+  has_many :tweets
+  validates :username, presence: true, length: { minimum: 3, maximum: 64 }
+  validates_uniqueness_of :username
+  validates :email, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 64 }
+  before_create :hash_password
 
-    private
-    def hash_password
-        self.password = BCrypt::Password.create(self.password)
-    end
+  private
+
+  def hash_password
+    self.password = BCrypt::Password.create(password)
+  end
 end
